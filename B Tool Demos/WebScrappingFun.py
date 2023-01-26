2a54e98a780bf88ada46428d66c755c7f745f894
Ben Puryear <54869170+Ben10164@users.noreply.github.com>
2023-01-26 09:26:18 -0800

    Added Research Proposal prompt

--- B Tool Demos/WebScrappingFun.py
+++ B Tool Demos/WebScrappingFun.py
@@ -5,9 +5,9 @@ from bs4 import BeautifulSoup
 
 url = "https://www.allmysportsteamssuck.com/ncaa-division-i-football-and-basketball-twitter-hashtags-and-handles/"
 
-
 def scrape_team_twitter_info():
     response = requests.get(url)
+    
     print(response.status_code)
     print(response.text)
 
